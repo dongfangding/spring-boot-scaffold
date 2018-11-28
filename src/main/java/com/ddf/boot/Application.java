a60b1e42c4b7785b7a0c1c65a478c1947990a97e
dongfang.ding <1041765757@qq.com>
2018-11-28 17:58:32 +0800

    项目初始化

---
+++ src/main/java/com/ddf/boot/Application.java
@@ -0,0 +1,15 @@
+package com.ddf.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DDf on 2018/11/28
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
